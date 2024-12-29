86b0d81ca601a289826e9f33e5d00d124c08b94c
Bilel Arfaoui <159537539+NakajimaAkemi@users.noreply.github.com>
2024-12-29 10:22:03 +0100

    Add files via upload

---
+++ GCP/3. base project/client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import os
+import json
+import datetime
+from google.cloud import pubsub_v1
+
+
+
+def callback(message):
+    message.ack()
+    msg = message.data.decode('utf-8')
+    ### here we can apply our filtering like if str(value) in msg
+    print(msg)
+ 
+
+if __name__=='__main__':    
+    #### Argument checks this is an example with cap
+    if len(argv)>1:
+        try:
+            cap = int(argv[1])
+            if cap < 0 or cap > 99999:
+                print("Argument error. Usage is:\n./"+argv[0]+" {int_value"+"}\nint_value must be in range(0,99999) (inclusive)")
+        except:
+            print("Argument error. Usage is:\n./"+argv[0]+" {int_value"+"}\nint_value must be in range(0,99999) (inclusive)")
+            quit()
+    
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path(PROJECT_ID, "NUOVO-CANTIERE")
+    pull = subscriber.subscribe(subscription_path, callback=callback)
+    try:
+        pull.result()
+    except Exception as e:
+        print(e)
+        pull.cancel()
